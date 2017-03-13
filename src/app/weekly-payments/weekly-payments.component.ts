0a005a141adf24361acf5a8fed313ccd9e39f7ed
Marvin Zumbado <marv_21@hotmail.com>
2017-03-12 19:15:07 -0600

    Weekly Balances

--- src/app/weekly-payments/weekly-payments.component.ts
+++ src/app/weekly-payments/weekly-payments.component.ts
@@ -1,9 +1,12 @@
 
 import { Component, ViewContainerRef, OnInit } from '@angular/core';
 import { ReportResponse } from './../models/api';
+import { ReportService } from './../services/report.service'
 import { AffiliateService } from './../services/affiliate.service'
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { Localization, LocaleService, TranslationService } from 'angular-l10n';
+import { IMyOptions } from 'mydatepicker';
+import { AuthService } from '../user/auth.service'
 
 @Component({
   selector: 'app-weekly-payments',
@@ -11,33 +14,59 @@ import { Localization, LocaleService, TranslationService } from 'angular-l10n';
   styleUrls: ['./weekly-payments.component.css']
 })
 export class WeeklyPaymentsComponent extends Localization implements OnInit {
-	response: any
+	private myDatePickerOptions: IMyOptions = {
+		// other options...
+		dateFormat: 'yyyy-mm-dd',
+	};
+	private dateModel: any
+	loading: boolean = false;
 
+	// ddlSports :string
+	ddlTransType: string = '-1'
+	ddlCurrency: string = '1'
+
+
+	public currencies = [
+		{ value: '1', display: 'USD' },
+		{ value: '2', display: 'MXN' },
+		{ value: '3', display: 'GBP' },
+		{ value: '4', display: 'EUR' }
+	];
+
+
+	response: any
 	errorMessage: string
-  
-  constructor(private affiliateService: AffiliateService, public toastr: ToastsManager, public vcr: ViewContainerRef, public locale: LocaleService, public translation: TranslationService) {
+
+	constructor(private affiliateService: AffiliateService, public toastr: ToastsManager, public vcr: ViewContainerRef,
+		public locale: LocaleService, public translation: TranslationService, private auth: AuthService) {
 		super(locale, translation);
 		this.toastr.setRootViewContainerRef(vcr);
-
 	}
 
+	ngOnInit() {
+		let currentDate = new Date();
+		let day = currentDate.getDate();
+		let month = currentDate.getMonth() + 1;
+		let year = currentDate.getFullYear();
+		this.dateModel = { date: { year: year, month: month, day: day } };
+	}
 
-ngOnInit() {
-		this.affiliateService.GetWeeklyPaymentsReport(1,'2017-02-02',"1")
+	go() {
+		this.loading = true;
+		let t0 = performance.now();
+		this.affiliateService.GetWeeklyPaymentsReport( this.auth.currentUser.id,
+		this.dateModel.date.year + '-' + this.dateModel.date.month + '-' + this.dateModel.date.day, this.ddlCurrency)
 			.subscribe(response => {
 				this.response = response;
-				if (this.response.ResponseStatus.Status === 'Success'
-					&& this.response.ResponseInfo) {
-					this.toastr.success('This toast will dismiss in 2 seconds.', 'Successssssssssssssssss');
-					console.log(this.response.ResponseInfo);
-
-				}
-				else this.toastr.error('This is not good!', 'Oops!');
+				this.loading = false;
+				let t1 = performance.now();
+				this.toastr.success('This query took ' + (t1 - t0) + ' milliseconds..', 'Success');
 			},
 			error => this.errorMessage = <any>error);
-
-
-
 	}
-
 }
+
+
+
+
+
