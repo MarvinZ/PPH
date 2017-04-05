77c133081ccb7ef4db516cf7c29af8f5206a8699
Marvin Zumbado <marv_21@hotmail.com>
2017-04-05 13:48:43 -0600

    changed website setting

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -21,7 +21,7 @@ export class AuthService {
 
   constructor(private router: Router, private http: Http) { }
   currentUser: IUser
-  siteName: string = 'Admin'//'PPH', 'Admin', 'Affiliates', 'Agent'
+  siteName: string = 'Affiliates'//'PPH', 'Admin', 'Affiliates', 'Agent'
   // loginUser(userName: string, password: string) { }
 
 
